7837c5a43512be986840b3504069055b9b8e73a4
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2022-03-26 00:46:29 +0530

    Greet Program

---
+++ src/com/company/Greet.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Greet {
+    public static void main(String[] args) {
+        System.out.print("Enter Your Name: ");
+        Scanner in = new Scanner(System.in);
+        String name = in.next();
+        String message ="Good Morning" + name + " It's Monday Today";
+        System.out.println(message);
+
+    }
+}
+
+
