43f59dd0e0b7986b5b1a78d233cfa6de3cbe8808
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2022-05-20 08:23:13 +0530

    loop

---
+++ src/com/company/loop.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class loop {
+    public static void main(String[] args) {
+//        for (int num = 1; num <= 5; num++){
+//            System.out.println(num);
+//        }
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+//
+//        for (int num  = 1; num <= n; num ++){
+//            System.out.println(num);
+//
+//        }
+
+
+//        int num = 1;
+//         while (num <= 5){
+//             System.out.println(num);
+//             num++;
+//         }
+
+
+        int n = 1;
+         do {
+             System.out.println(n);
+             n++;
+         }while (n<=10);
+
+
+
+    }
+}
